60146c7f2a0e15de51054de13acb5653b8150f79
iwdie <42923352+iwdie@users.noreply.github.com>
2024-07-06 12:43:24 +0530

    :)another one

--- New Game Project/main_quiz.gd
+++ New Game Project/main_quiz.gd
@@ -7,6 +7,8 @@ signal correctans()
 signal wrongans()
 signal buttonpreesed()
 var question_activation = true
+var current_health = 100
+@onready var demonhealth = $Monster/demonhealth
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -26,12 +28,12 @@ func _process(delta):
 		$TouchScreenButton3/RichTextLabel.set_text("Germination")
 		$TouchScreenButton4/RichTextLabel.set_text("Seedling")
 	elif questionno == 2:
-		correct_option =3
-		$"DialogueBorder-01/RichTextLabel".set_text(" Which species young are described as Blackbacks?")
-		$TouchScreenButton1/RichTextLabel.set_text("Chimpanzees")
-		$TouchScreenButton2/RichTextLabel.set_text("Brown Bears")
-		$TouchScreenButton3/RichTextLabel.set_text("Gorillas")
-		$TouchScreenButton4/RichTextLabel.set_text("Jaguars")
+		correct_option =4
+		$"DialogueBorder-01/RichTextLabel".set_text("The Arctic warmed by how many degrees Celsius between 1971 and 2019?")
+		$TouchScreenButton1/RichTextLabel.set_text("0.1")
+		$TouchScreenButton2/RichTextLabel.set_text("1.1")
+		$TouchScreenButton3/RichTextLabel.set_text("2.1")
+		$TouchScreenButton4/RichTextLabel.set_text("3.1")
 	elif questionno == 3:
 		correct_option = 4
 		$"DialogueBorder-01/RichTextLabel".set_text("The elephant is the heaviest land animal in the world. Weighing up to almost 8,000 pounds, which is the second heaviest land animal?")
@@ -100,7 +102,150 @@ func _process(delta):
 		$TouchScreenButton3/RichTextLabel.set_text("Sugarcane")
 		$TouchScreenButton4/RichTextLabel.set_text("Barley")
 		
+	elif questionno == 11:
+		correct_option = 3
+		$"DialogueBorder-01/RichTextLabel".set_text("If global temperatures rise by 1.5%, can you guess what percentage of the world’s wildlife will be at risk of extinction?")
+		$TouchScreenButton1/RichTextLabel.set_text("5-10%")
+		$TouchScreenButton2/RichTextLabel.set_text("10-20%")
+		$TouchScreenButton3/RichTextLabel.set_text("20-30%")
+		$TouchScreenButton4/RichTextLabel.set_text("30-40%")
 		
+	elif questionno == 12:
+		correct_option = 4
+		$"DialogueBorder-01/RichTextLabel".set_text("Our planet has already lost nearly 40% of its forests in the last three centuries. How many football pitches is that a minute?")
+		$TouchScreenButton1/RichTextLabel.set_text("1 football pitch")
+		$TouchScreenButton2/RichTextLabel.set_text("4 football pitch")
+		$TouchScreenButton3/RichTextLabel.set_text("3 football pitch")
+		$TouchScreenButton4/RichTextLabel.set_text("5 football pitch")
+		
+	elif questionno == 13:
+		correct_option = 4
+		$"DialogueBorder-01/RichTextLabel".set_text("Which big cat can’t roar?")
+		$TouchScreenButton1/RichTextLabel.set_text("Lion")
+		$TouchScreenButton2/RichTextLabel.set_text("Tiger")
+		$TouchScreenButton3/RichTextLabel.set_text("Jaguar")
+		$TouchScreenButton4/RichTextLabel.set_text("Snow leopard")
+		
+	elif questionno == 14:
+		correct_option = 3
+		$"DialogueBorder-01/RichTextLabel".set_text("Which is the world’s rarest big cat?")
+		$TouchScreenButton1/RichTextLabel.set_text("Tiger")
+		$TouchScreenButton2/RichTextLabel.set_text("Snow leopard")
+		$TouchScreenButton3/RichTextLabel.set_text("Amur leopard")
+		$TouchScreenButton4/RichTextLabel.set_text("Cheetah")
+		
+	elif questionno == 15:
+		correct_option = 2
+		$"DialogueBorder-01/RichTextLabel".set_text(" How far can a tiger leap?")
+		$TouchScreenButton1/RichTextLabel.set_text("2 metres")
+		$TouchScreenButton2/RichTextLabel.set_text("5 metres")
+		$TouchScreenButton3/RichTextLabel.set_text("8 metres")
+		$TouchScreenButton4/RichTextLabel.set_text("10 metres")
+		
+		
+	elif questionno == 16:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("The word jaguar means")
+		$TouchScreenButton1/RichTextLabel.set_text("He who kills with one leap")
+		$TouchScreenButton2/RichTextLabel.set_text("He who scares with one roar")
+		$TouchScreenButton3/RichTextLabel.set_text("He who attacks with one claw")
+		$TouchScreenButton4/RichTextLabel.set_text("He who dares, wins")
+		
+	elif questionno == 17:
+		correct_option = 4
+		$"DialogueBorder-01/RichTextLabel".set_text("How do African Wild Dogs vote within their packs?")
+		$TouchScreenButton1/RichTextLabel.set_text("By high-fiving")
+		$TouchScreenButton2/RichTextLabel.set_text("By spreading their scent")
+		$TouchScreenButton3/RichTextLabel.set_text("By howling")
+		$TouchScreenButton4/RichTextLabel.set_text("By sneezing")
+		
+		
+	elif questionno == 18:
+		correct_option = 2
+		$"DialogueBorder-01/RichTextLabel".set_text("Plastic discarded in UK waters floats through currents to the Arctic. Approximately how many years does it take to drift there?")
+		$TouchScreenButton1/RichTextLabel.set_text("1 year")
+		$TouchScreenButton2/RichTextLabel.set_text("2 years")
+		$TouchScreenButton3/RichTextLabel.set_text("3 years")
+		$TouchScreenButton4/RichTextLabel.set_text("5 years")
+		
+		
+	elif questionno == 19:
+		correct_option = 3
+		$"DialogueBorder-01/RichTextLabel".set_text("Polar bears have large paws which they use like paddles to help them swim. The paws also distribute the bear’s weight over thin ice, so they don't fall through. How wide are the paws of an adult polar bear?")
+		$TouchScreenButton1/RichTextLabel.set_text("Under 10 cm")
+		$TouchScreenButton2/RichTextLabel.set_text("10-15 cm")
+		$TouchScreenButton3/RichTextLabel.set_text("Up to 30 cm")
+		$TouchScreenButton4/RichTextLabel.set_text("at least 40 cm")
+		
+		
+	elif questionno == 20:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("In 2018, a young female Arctic fox crossed the ice from Norway's Svalbard islands to northern Canada in an epic journey, covering 3,506 km (2,176 miles). Approximately, how long did it take the fox?")
+		$TouchScreenButton1/RichTextLabel.set_text("10 days")
+		$TouchScreenButton2/RichTextLabel.set_text("2.5 months")
+		$TouchScreenButton3/RichTextLabel.set_text("6 months")
+		$TouchScreenButton4/RichTextLabel.set_text("9 months")
+		
+		
+	elif questionno == 21:
+		correct_option = 3
+		$"DialogueBorder-01/RichTextLabel".set_text(" Which species young are described as Blackbacks?")
+		$TouchScreenButton1/RichTextLabel.set_text("Chimpanzees")
+		$TouchScreenButton2/RichTextLabel.set_text("Brown Bears")
+		$TouchScreenButton3/RichTextLabel.set_text("Gorillas")
+		$TouchScreenButton4/RichTextLabel.set_text("Jaguars")
+	
+	elif questionno == 22:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("Which habitat covers around one-third of the Earth's land, and sequesters 11.4 billion tonnes of carbon each year?")
+		$TouchScreenButton1/RichTextLabel.set_text("Forests")
+		$TouchScreenButton2/RichTextLabel.set_text("Deserts")
+		$TouchScreenButton3/RichTextLabel.set_text("Grasslands")
+		$TouchScreenButton4/RichTextLabel.set_text("Swampland")
+		
+		
+	elif questionno == 23:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("")
+		$TouchScreenButton1/RichTextLabel.set_text("")
+		$TouchScreenButton2/RichTextLabel.set_text("")
+		$TouchScreenButton3/RichTextLabel.set_text("")
+		$TouchScreenButton4/RichTextLabel.set_text("")
+		
+		
+	elif questionno == 24:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("")
+		$TouchScreenButton1/RichTextLabel.set_text("")
+		$TouchScreenButton2/RichTextLabel.set_text("")
+		$TouchScreenButton3/RichTextLabel.set_text("")
+		$TouchScreenButton4/RichTextLabel.set_text("")
+		
+		
+	elif questionno == 25:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("")
+		$TouchScreenButton1/RichTextLabel.set_text("")
+		$TouchScreenButton2/RichTextLabel.set_text("")
+		$TouchScreenButton3/RichTextLabel.set_text("")
+		$TouchScreenButton4/RichTextLabel.set_text("")
+		
+		
+	elif questionno == 26:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("")
+		$TouchScreenButton1/RichTextLabel.set_text("")
+		$TouchScreenButton2/RichTextLabel.set_text("")
+		$TouchScreenButton3/RichTextLabel.set_text("")
+		$TouchScreenButton4/RichTextLabel.set_text("")
+		
+	elif questionno == 27:
+		correct_option = 1
+		$"DialogueBorder-01/RichTextLabel".set_text("")
+		$TouchScreenButton1/RichTextLabel.set_text("")
+		$TouchScreenButton2/RichTextLabel.set_text("")
+		$TouchScreenButton3/RichTextLabel.set_text("")
+		$TouchScreenButton4/RichTextLabel.set_text("")
 		
 		
 	else:
@@ -117,7 +262,9 @@ func _on_correctans():
 	$CharacterBody2D/AnimatedSprite2D.play("attack")
 	print("correct")
 	$AudioStreamPlayer2D2.play()
-	score += 10
+	if current_health > 0:
+		current_health -= 10  # Decrease health by 10
+		demonhealth.value = current_health
 	#Global.score += 1         
 				   
 	$ColorRect.set_color(Color(0.7,0.7,0.9))
@@ -235,4 +382,5 @@ func _on_touch_screen_button_1_pressed():
 	selected_option = 1
 	if question_activation:
 		buttonpreesed.emit()
+		
 		 # Replace with function body.
